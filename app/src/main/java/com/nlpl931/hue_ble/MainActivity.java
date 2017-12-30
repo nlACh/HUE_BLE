19dc3d749049fab260d494d024a1c27d9290d640
Nilayan Paul <nlpl931@gmail.com>
2017-12-30 00:31:01 -0800

    finalized!!
    Face it the app looks hideous.

--- app/src/main/java/com/nlpl931/hue_ble/MainActivity.java
+++ app/src/main/java/com/nlpl931/hue_ble/MainActivity.java
@@ -11,6 +11,8 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
@@ -26,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
     public String addr = "98:D3:33:80:68:00";
     private static final UUID uid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
+    //For the seek-bars
+    SeekBar sR, sG, sB;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,14 +39,61 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(R.string.app_name);
+        sR = findViewById(R.id.red);
+        sG = findViewById(R.id.green);
+        sB = findViewById(R.id.blue);
+
         Button set = findViewById(R.id.set);
+        Button cycle = findViewById(R.id.cycle);
+        Button white = findViewById(R.id.white);
+        Button off = findViewById(R.id.off);
+
         set.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (isConnected)
-                    sendData("1223--0");
+                if (isConnected) {
+                    int red = sR.getProgress();
+                    int green = sG.getProgress();
+                    int blue = sB.getProgress();
+                    StringBuilder sb = new StringBuilder();
+                    sb.append('#');
+                    sb.append(red);
+                    sb.append('#');
+                    sb.append(green);
+                    sb.append('#');
+                    sb.append(blue);
+                    sb.append('#');
+                    sb.append('0');
+                    TextView tv = findViewById(R.id.about);
+                    tv.setText(sb.toString());
+                    sendData(sb.toString());
+                }
                 else
                     Toast.makeText(getBaseContext(), "Connect first!!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        cycle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isConnected)
+                    sendData("@C0");
+            }
+        });
+
+        white.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isConnected)
+                    sendData("@W0");
+            }
+        });
+
+        off.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isConnected)
+                    sendData("@O0");
             }
         });
     }
@@ -134,14 +186,17 @@ public class MainActivity extends AppCompatActivity {
         }
         if (id == R.id.disconnect)
         {
-            try{
-                bs.close();
-                isConnected = false;
-            }catch (IOException es)
-            {
-                Toast.makeText(getBaseContext(), es.toString(), Toast.LENGTH_LONG).show();
-                finish();
-            }
+            if (isConnected){
+                try{
+                    bs.close();
+                    isConnected = false;
+                }catch (IOException es)
+                {
+                    Toast.makeText(getBaseContext(), es.toString(), Toast.LENGTH_LONG).show();
+                    finish();
+                }
+            }else
+                Toast.makeText(getBaseContext(), "Connect first", Toast.LENGTH_SHORT).show();
         }
 
         return super.onOptionsItemSelected(item);
